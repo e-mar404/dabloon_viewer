55cfa1a6f02dce82d7ec3ac07fb18b3b1ba9972a
e-mar404 <egmarinn@cougarnet.uh.edu>
2022-11-30 22:40:05 -0600

    Delete Boilerplate code and add sign up component (not functional)

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app"
+import "firebase/auth"
+
+// using env file so we can use development env variables and production env variables without changing code
+const app = firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+})
+
+export const auth = app.auth()
+export default app
